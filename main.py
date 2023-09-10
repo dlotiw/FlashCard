3dae0e4ddce3cb99110c2aed69dd01054aba8e7f
Witold Brzykcy <witold.brzykcy@ukw.edu.pl>
2023-09-10 15:48:47 +0200

    Changed a way cards are working

--- main.py
+++ main.py
@@ -7,51 +7,71 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 #----------Creating FlashCard----#
 
-def add_flash_card(type):
-    try:
-        data = pandas.read_csv("FlashCard/data/spanish_words_200.csv")
-    except FileNotFoundError:
-        messagebox.askretrycancel("No words to learn file found","Not found words list, make sure there is espanol_words.csv file in data folder and try again")
-    words = data.to_dict(orient="records")
-    canvas_text=random.choice(words)
-    if type == 'es':
-        canvas.itemconfig(word,text=canvas_text['Spanish'])
-    elif type == 'en':
-        canvas.itemconfig(word,text=canvas_text['English'])
+class Card:
+    def __init__(self, language) -> None:
+        self.front = PhotoImage(file="FlashCard/images/card_front.png")
+        self.back = PhotoImage(file="FlashCard/images/card_back.png")
+        self.image = self.front
+        self.language = language
+        self.word_spanish = ""
+        self.word_english = ""
+        self.get_word()
+        
+
+    def flip_card(self):
+        if self.image == self.front:
+            self.image = self.back
+            self.language = "English"
+        else:
+            self.image = self.front
+            self.language = "Spanish"
+    
+    def get_word(self):
+        try:
+            data = pandas.read_csv("FlashCard/data/spanish_words_200.csv")
+        except FileNotFoundError:
+            messagebox.askretrycancel("No words to learn file found","Not found words list, make sure there is espanol_words.csv file in data folder and try again")
+        words = data.to_dict(orient="records")
+        text=random.choice(words)
+        self.word_spanish = text['Spanish']
+        self.word_english = text['English']
+
+    
+def change():
+    cardo.flip_card()
+    canvas.itemconfig(canvas_image,image=cardo.image)
+    canvas.itemconfig(word,text=cardo.word_english,fill="white")
+    canvas.itemconfig(language,text=cardo.language,fill="white")
+
+def new_card():
+    global cardo
+    cardo = Card("Spanish")
+    canvas.itemconfig(canvas_image,image=cardo.image)
+    canvas.itemconfig(language,text=cardo.language,fill='black')
+    canvas.itemconfig(word,text=cardo.word_spanish,fill='black')
         
 #----------Buttons Click-------#
-def right_button_click():
-    add_flash_card('es')
-    flip_card(0)
-
+def righ_button_click():
+    new_card()
+    screen.after(3000,change)
 
 def wrong_button_click():
-    add_flash_card('en')
-    flip_card(1)
+    new_card()
+    screen.after(3000,change)
 
-def flip_card(side):
-    if side == 1:
-        canvas.itemconfig(canvas_image,image=card_back)
-        canvas.itemconfig(language,text='English',fill='white')
-        canvas.itemconfig(word,fill='white')
-    if side == 0:
-        canvas.itemconfig(canvas_image,image=card_front)
-        canvas.itemconfig(language,text ='Spanish',fill='black')
-        canvas.itemconfig(word,fill='black')
 #---------------UI--------------#
 
 #Main window
 screen = Tk()
 screen.title("FlashCards")
 screen.config(padx=50,pady=50,background=BACKGROUND_COLOR)
+cardo = Card("Spanish")
 
 #FlashCard
 canvas = Canvas(width=800,height=526,background=BACKGROUND_COLOR,highlightthickness=0)
-card_front = PhotoImage(file="FlashCard/images/card_front.png")
-card_back = PhotoImage(file="FlashCard/images/card_back.png")
-canvas_image = canvas.create_image(400,263,image=card_front)
-language = canvas.create_text(400,150,text="Spanish",font=("Ariel",40,"italic"))
-word = canvas.create_text(400,263,text="tekst",font=("Ariel",60,"bold"))
+canvas_image = canvas.create_image(400,263,image=cardo.front)
+language = canvas.create_text(400,150,text=cardo.language,font=("Ariel",40,"italic"))
+word = canvas.create_text(400,263,text=cardo.word_spanish,font=("Ariel",60,"bold"))
 canvas.grid(row=0,column=0,columnspan=2)
 
 
@@ -62,9 +82,10 @@ wrong_button.grid(row=1,column=0)
 
 #Right button
 right_image = PhotoImage(file="FlashCard/images/correct.png")
-right_button = Button(image=right_image,background=BACKGROUND_COLOR,highlightthickness=0,activebackground=BACKGROUND_COLOR,bd=0,command=right_button_click)
+right_button = Button(image=right_image,background=BACKGROUND_COLOR,highlightthickness=0,activebackground=BACKGROUND_COLOR,bd=0,command=righ_button_click)
 right_button.grid(row=1,column=1)
 
-
+screen.after(3000,change)
 
 screen.mainloop()
+
